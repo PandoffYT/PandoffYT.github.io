916d20ac70b987830fddc1833dd09715a49c3f93
Pando_YT <99325649+PandoffYT@users.noreply.github.com>
2023-10-29 16:03:46 +0100

    php added (handsome)

---
+++ api/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>api</title>
+</head>
+<body>
+
+    <font face="arial">
+
+        <a target="new_tab" href="./api_img.php">api_img</a>
+
+
+    </font>
+    
+</body>
+</html>
