5eac1d3a2fedff67e378bcee6f48fbf770627a16
Pando_YT <99325649+PandoffYT@users.noreply.github.com>
2024-08-30 19:22:00 +0200

    js modified

--- ressources/ext/phoneapp/jsonview.js
+++ ressources/ext/phoneapp/jsonview.js
@@ -10,3 +10,44 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error fetching JSON data:", error));
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const jsonFilePath = 'verhistory.json';
+    const selectElement = document.getElementById('verSelect');
+    const downloadbtn = document.getElementById('downloadbtn');
+
+    fetch(jsonFilePath)
+        .then(response => response.json())
+        .then(data => {
+            const ver = data.ver;
+
+
+            ver.forEach(ver => {
+                const option = document.createElement('option');
+                option.value = ver.link; // Use the image URL as the value
+                option.textContent = ver.version + ver.vername;
+                selectElement.appendChild(option);
+            });
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
+
+    selectElement.addEventListener('change', function() {
+        downloadbtn.innerHTML = '';
+
+        const selectedver = selectElement.value;
+
+        const downloadButton = document.createElement('a');
+        downloadButton.href = selectedver;
+        downloadButton.download = selectedver.split('/').pop();
+        downloadButton.textContent = 'Download APK';
+        downloadButton.style.display = 'inline-block';
+        downloadButton.style.marginTop = '10px';
+        downloadButton.style.padding = '10px';
+        downloadButton.style.backgroundColor = '#007BFF';
+        downloadButton.style.color = '#fff';
+        downloadButton.style.textDecoration = 'none';
+        downloadButton.style.borderRadius = '5px';
+
+        downloadbtn.appendChild(downloadButton);
+    });
+});
